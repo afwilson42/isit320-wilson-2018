82e18197910e8965d67558d50766ee93afacafe4
Andrew Wilson <andrewf.wilson@bellevuecollege.edu>
2018-10-31 17:18:54 -0700

    more work on system check

--- week07-system-check-refactor/client/src/App.js
+++ week07-system-check-refactor/client/src/App.js
@@ -4,22 +4,56 @@ import './App.css';
 
 class App extends Component {
     render() {
+        const radioWeb = (
+            <div className="container">
+                <form onSubmit={this.handleSubmit}>
+                    <fieldset>
+                        <div className="elf-form-field">
+
+                            <legend>Services</legend>
+                            <input
+                                type="radio"
+                                name="app-choice"
+                                data-endpoint="0"
+                                value="CpuInfo"
+                                id="elf-radio-cpu"
+                                onChange={this.handleChange}
+                            />
+                            <label htmlFor="elf-radio-cpu">CpuInfo</label>
+
+                            <input
+                                type="radio"
+                                name="app-choice"
+                                data-endpoint="0"
+                                value="VersionCheck"
+                                id="elf-radio-version"
+                                onChange={this.handleChange}
+                            />
+                            <label htmlFor="elf-radio-version">Version Info</label>
+
+                        </div>
+
+                        <div className="form-group">
+                            <button type="submit" className="btn btn-primary">Run System Script</button>
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
+        );
         return (
             <div className="App">
                 <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
+                    <h1>System Check</h1>
                 </header>
+                <main>
+                    <section>
+                        {radioWeb}
+                    </section>
+                    <section>
+                        <pre>{this.state.allData}</pre>
+                    </section>
+                    <button onClick={this.runFoo}>Run Foo</button>
+                </main>
             </div>
         );
     }
