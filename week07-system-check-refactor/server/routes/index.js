126c20393884685726823a64994325c7f1bc1a93
Andrew Wilson <andrewf.wilson@bellevuecollege.edu>
2018-11-26 18:24:03 -0800

    Completed fixes for System Check Refactor for week07-system-check-refactor_2 on branch week07 with tag v1.0.13.

--- week07-system-check-refactor/server/routes/index.js
+++ week07-system-check-refactor/server/routes/index.js
@@ -4,8 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) {
     'use strict';
-    res.render('index',
-        { title: 'System Check Refactor Server'});
+    res.render('index', { title: 'System Check Refactor Server' });
 });
 
 module.exports = router;
