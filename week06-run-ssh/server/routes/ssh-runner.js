7b2bb9982b416b5333c83a51dddd9733f0dd80cd
Andrew Wilson <andrewf.wilson@bellevuecollege.edu>
2018-12-05 16:13:52 -0800

    no longer hard coding IP and Private Key for week06-run-ssh, Midterm and newer, NOT on SystemCheckRefactor on branch week06 with tag v1.0.16.

--- week06-run-ssh/server/routes/ssh-runner.js
+++ week06-run-ssh/server/routes/ssh-runner.js
@@ -1,15 +1,14 @@
 var express = require('express');
 var router = express.Router();
 const Client = require('ssh2').Client;
-
-const hostAddress = '35.175.11.116';
+const elfUtils = require('elven-code').elfUtils;
 
 let allData = '';
 
-const runCpuInfo = (hostAddress, response) => {
+const runCpuInfo = (ssHip,ssHidentity, response) => {
     var conn = new Client();
+    console.log('Client :: ready', ssHip, ssHidentity);
     conn.on('ready', function() {
-        console.log('Client :: ready');
         conn.exec('~/CpuInfo', function(err, stream) {
             if (err) throw err;
             stream
@@ -33,17 +32,59 @@ const runCpuInfo = (hostAddress, response) => {
                 });
         });
     }).connect({
-        host: hostAddress,
+        host: ssHip,
         port: 22,
         username: 'ubuntu',
         privateKey: require('fs').readFileSync(
-            process.env.HOME + '/.ssh/ec2-isit320-wilson.pem'
+            process.env.HOME + '/.ssh/' + ssHidentity
         )
+    });
+
+
+};
+
+const getSshIp = () => {
+    return new Promise(function (resolve, reject) {
+        elfUtils.readFile(process.env.HOME + '/.ssh/config')
+            .then((content) => {
+                //var pattern = new RegExp('Host ec2-bc[\\s\\S]\\s*(.*)[\\s\\S]\\s*(.*)[\\s\\S]\\s*(.*)[\\s\\S]\\s*(.*)');
+                var pattern = new RegExp('Host ec2-bc\n\t(.*)\n\t(.*)\n\t(.*)\n\t(.*)');
+                const result = {};
+                const match = content.result.match(pattern);
+                for (let i = 1; i < 5; i++) {
+                    if (match[i].startsWith('HostName')) {
+                        var hostPattern = new RegExp('HostName\\s(.*)');
+                        result.hostName = match[i].match(hostPattern)[1];
+                    }
+                    if (match[i].startsWith('IdentityFile')) {
+                        const idPattern = new RegExp('IdentityFile\\s(.*)');
+                        const path = match[i].match(idPattern)[1];
+                        result.identityFile = path.substring(path.lastIndexOf('/') + 1, path.length)
+                    }
+                }
+                resolve(result);
+            })
+            .catch(reject);
     });
 };
 
-router.get('/call-cpu-info', (request, response) => {
+/*router.get('/call-cpu-info', (request, response) => {
     runCpuInfo(hostAddress, response);
+}); */
+
+router.get('/call-cpu-info', function(request, response) {
+    console.log('runCpuInfo called in ssh-runner');
+    getSshIp()
+        .then((result) => {
+            console.log(result);
+            runCpuInfo(result.hostName, result.identityFile, response);
+        })
+        .catch((err) => {
+            response.send(err);
+        });
+
 });
+
+
 
 module.exports = router;
