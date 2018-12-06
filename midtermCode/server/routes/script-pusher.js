68ed7f625e1e3d29ee8706990f4c14994955db36
Andrew Wilson <andrewf.wilson@bellevuecollege.edu>
2018-12-05 18:07:01 -0800

    more work on midterm

--- midtermCode/server/routes/script-pusher.js
+++ midtermCode/server/routes/script-pusher.js
@@ -3,6 +3,22 @@ var router = express.Router();
 
 const spawn = require('child_process').spawn;
 
+const check = (request, response, next) => {
+    console.log('REQUEST CHECK CALLED', request.query);
+    const validOptions = ['CpuInfo', 'VersionCheck', 'uptime'];
+    if (request.query.script) {
+        console.log('INSIDE REQUEST SCRIPT');
+        if (!validOptions.includes(request.query.script)) {
+            console.log('INSIDE REQUEST INVALID OPTION');
+            response.send({result: 'error', error: 'Invalid Option: ' + request.query.script, script: request.query.script});
+            return;
+        }
+    }
+    next();
+};
+
+router.use(check);
+
 const copyFile = () => {
     return new Promise(function(resolve, reject) {
         let allData = '';
